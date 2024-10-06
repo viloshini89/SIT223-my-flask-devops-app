37382ca3c6d2e60c2de767309c301f7ca56c8917
viloshini89 <127585383+viloshini89@users.noreply.github.com>
2024-10-06 20:52:05 +0800

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -6,13 +6,13 @@ import pytest
 from app import app
 
 @pytest.fixture
-def test_client():
+def flask_test_client():
     """Create a test client for the Flask app."""
     app.config['TESTING'] = True
     with app.test_client() as client:
         yield client
 
-def test_home(test_client):
+def test_home(flask_test_client):
     """Test the home page route."""
-    response = test_client.get('/')
+    response = flask_test_client.get('/')
     assert response.data == b'Hello, DevOps World!'
